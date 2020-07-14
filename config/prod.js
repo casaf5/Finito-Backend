2c819bc48198beb1142c2b132dad8439daa03369
Asaf Cohen <casaf5@gmail.com>
2020-07-14 23:10:26 +0300

    now

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "dbURL": "mongodb+srv://casaf5:Haris2020@cluster0.n9j6d.mongodb.net/mstoy?retryWrites=true&w=majority"
+  "dbURL": "mongodb+srv://casaf5:Haris2020@cluster0.n9j6d.mongodb.net/msToy?retryWrites=true&w=majority"
 }
