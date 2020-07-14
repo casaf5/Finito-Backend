c5ef274cc270292aca83ce67c09c4fcfc463e351
Asaf Cohen <casaf5@gmail.com>
2020-07-14 18:51:53 +0300

    connected to atlas

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "dbURL": "mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority",
+  "dbURL": "mongodb+srv://casaf5:EbH50L073TcGGxwe@cluster0.n9j6d.mongodb.net/?retryWrites=true&w=majority",
 }
