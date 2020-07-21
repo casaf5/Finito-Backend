e49783036cd11e4379c89f229062fc207d97d3e6
Tom <tomer.tsuker@gmail.com>
2020-07-21 22:49:29 +0900

    added basic socket.io functionailty

--- api/board/board.route.js
+++ api/board/board.route.js
@@ -1,13 +1,21 @@
-const express=require('express')
-const {getBoards,getBoard,removeBoard,updateBoard,addBoard}=require('./board.controller')
-const {requireAuth,requireAdmin}  = require('../../middlewares/requireAuth.middleware')
-const router=express.Router()
+const express = require("express");
+const {
+  getBoards,
+  getBoard,
+  removeBoard,
+  updateBoard,
+  addBoard,
+} = require("./board.controller");
+const {
+  requireAuth,
+  requireAdmin,
+} = require("../../middlewares/requireAuth.middleware");
+const router = express.Router();
 
-router.get('/',getBoards)
-router.get('/:id',getBoard)
-router.delete('/:id',requireAuth,requireAdmin,removeBoard)
-router.put('/:id',requireAuth,requireAdmin,updateBoard)
-router.post('/',requireAuth,requireAdmin,addBoard)
+router.get("/", getBoards);
+router.get("/:id", getBoard);
+router.delete("/:id", removeBoard);
+router.put("/:id", updateBoard);
+router.post("/", addBoard);
 
-module.exports=router
-
+module.exports = router;
--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -1,20 +1,20 @@
-
-module.exports = connectSockets
+module.exports = connectSockets;
+const BoardService = require("../board/board-service.js");
 
 function connectSockets(io) {
-    io.on('connection', socket => {
-        socket.on('chat newMsg', msg=>{
-            console.log(msg)
-            // io.emit('chat addMsg', msg)
-            // emits only to sockets in the same room
-            io.to(socket.myTopic).emit('chat addMsg', msg)
-        })
-        socket.on('chat topic', topic=>{
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
-            }
-            socket.join(topic)
-            socket.myTopic = topic;
-        })
-    })
+  io.on("connection", (socket) => {
+    socket.on("joinedBoard", (boardId) => {
+      if (socket.board) {
+        socket.leave(socket.board);
+      }
+      socket.board = boardId;
+      console.log("socket belongs to this board:", socket.board);
+      socket.join(boardId);
+    });
+
+    socket.on("boardUpdate", async (board) => {
+      await BoardService.update(board);
+      io.to(socket.board).emit("boardUpdate", board);
+    });
+  });
 }
