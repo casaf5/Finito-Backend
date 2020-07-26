efb6a56c302e9f8ddf6f953b2c662046c2974e24
Asaf Cohen <casaf5@gmail.com>
2020-07-26 11:48:42 +0300

    worrrrkk

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "dbURL": "mongodb+srv://casaf5:Haris2020@cluster0.n9j6d.mongodb.net/finitoDB?retryWrites=true&w=majority"
+  "dbURL": "mongodb+srv://casaf5:Haris2021@cluster0.n9j6d.mongodb.net/finitoDB?retryWrites=true&w=majority"
 }
 
 
