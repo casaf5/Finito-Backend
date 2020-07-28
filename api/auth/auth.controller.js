73de281150f7edc7b174ec009f5b5b922a7cd3cc
Asaf Cohen <casaf5@gmail.com>
2020-07-28 10:37:10 +0300

    changes

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -3,7 +3,6 @@ const logger = require('../../services/logger.service')
 
 async function login(req, res) {
     const { userName, password } = req.body
-    console.log(userName,password)
     try {
         const user = await authService.login(userName, password)
         req.session.user = user;
