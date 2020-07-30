080a10b6e5068748655d7793c3ad4d5c72a485b6
Asaf Cohen <casaf5@gmail.com>
2020-07-31 00:46:14 +0300

    auth fix

--- api/board/board.route.js
+++ api/board/board.route.js
@@ -1,15 +1,6 @@
 const express = require("express");
-const {
-  getBoards,
-  getBoard,
-  removeBoard,
-  updateBoard,
-  addBoard,
-} = require("./board.controller");
-const {
-  requireAuth,
-  requireAdmin,
-} = require("../../middlewares/requireAuth.middleware");
+const { getBoards, getBoard, removeBoard,updateBoard,addBoard,} = require("./board.controller");
+const { requireAuth,requireAdmin,} = require("../../middlewares/requireAuth.middleware");
 const router = express.Router();
 
 
